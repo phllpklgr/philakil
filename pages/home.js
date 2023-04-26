e39a4d3e5c9ca6b9bfe9b7a2f79ab141eadd2254
Phillip Kilgore <phillip.kilgore15@gmail.com>
2023-04-25 20:23:45 -0400

    updated js functionality

--- pages/home.js
+++ pages/home.js
@@ -7,4 +7,22 @@ let currentIndex = 0;
 carouselControls.forEach((control) => {
   control.addEventListener('click', () => {
     if (control.classList.contains('carousel-prev')) {
-      currentIndex = (currentIndex === 0) ? carouselImages.length
+      currentIndex = (currentIndex === 0) ? carouselImages.length - 1 : currentIndex - 1;
+    } else if (control.classList.contains('carousel-next')) {
+      currentIndex = (currentIndex === carouselImages.length - 1) ? 0 : currentIndex + 1;
+    }
+
+    // Move the carousel to the new index
+    const carouselWidth = carousel.offsetWidth;
+    carousel.style.transform = `translateX(-${currentIndex * carouselWidth}px)`;
+
+    // Update the active class on the carousel images
+    carouselImages.forEach((image, index) => {
+      if (index === currentIndex) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
+  });
+});
