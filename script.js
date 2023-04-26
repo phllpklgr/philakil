4314826bdc6f4278d69dc39074d04f686725b42e
Phillip Kilgore <phillip.kilgore15@gmail.com>
2023-04-25 20:57:37 -0400

    restyling the enter the site page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function enterSite() {
-    window.location.href = "pages/";
+function hideElements() {
+    const elementsToHide = document.querySelectorAll('body > *:not(#page-text)');
+    for (let i = 0; i < elementsToHide.length; i++) {
+        elementsToHide[i].style.display = 'none';
+    }
+    const pageText = document.getElementById('page-text');
+    pageText.style.animation = 'slide-out 1s ease-in-out forwards';
+    setTimeout(() => {
+        window.location.href = 'main.html';
+    }, 1000);
 }
