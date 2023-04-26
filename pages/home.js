6da04af4719fa339f93ed06c5772d2f7f51dcab8
Phillip Kilgore <phillip.kilgore15@gmail.com>
2023-04-25 20:40:47 -0400

    updated carousel

--- pages/home.js
+++ pages/home.js
@@ -1,28 +1,39 @@
 const carousel = document.querySelector('.carousel');
 const carouselImages = document.querySelectorAll('.carousel-image');
-const carouselControls = document.querySelectorAll('.carousel-control');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
 let currentIndex = 0;
 
-// Set up click event listeners for carousel controls
-carouselControls.forEach((control) => {
-  control.addEventListener('click', () => {
-    if (control.classList.contains('carousel-prev')) {
-      currentIndex = (currentIndex === 0) ? carouselImages.length - 1 : currentIndex - 1;
-    } else if (control.classList.contains('carousel-next')) {
-      currentIndex = (currentIndex === carouselImages.length - 1) ? 0 : currentIndex + 1;
-    }
-
-    // Move the carousel to the new index
-    const carouselWidth = carousel.offsetWidth;
-    carousel.style.transform = `translateX(-${currentIndex * carouselWidth}px)`;
-
-    // Update the active class on the carousel images
-    carouselImages.forEach((image, index) => {
-      if (index === currentIndex) {
-        image.classList.add('active');
-      } else {
-        image.classList.remove('active');
-      }
-    });
-  });
+// Set up click event listener for previous arrow
+carouselPrev.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = carouselImages.length - 1;
+  }
+  updateCarousel();
 });
+
+// Set up click event listener for next arrow
+carouselNext.addEventListener('click', () => {
+  if (currentIndex < carouselImages.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  updateCarousel();
+});
+
+// Update carousel based on current index
+function updateCarousel() {
+  carouselImages.forEach((image, index) => {
+    if (index === currentIndex) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+// Initialize carousel with first image active
+updateCarousel();
